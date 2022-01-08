65878aca17fbdab0646433445a47b8be9cf8ffba
unknown <rita.saroufim@hotmail.com>
2022-01-08 19:03:50 +0200

    add: initialCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Test1 from './Test1';
+import Test2 from './Test2';
+import Test3 from './Test3';
+import Test4 from './Test4';
+import Test5 from './Test5';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    
+    return (
+      <div className="App">    
+        <Test1 />
+        <Test2 />
+        <Test3 />
+        <Test4 />
+        <Test5 />    
+      </div>
+      );
+    }
+    
 }
 
 export default App;
---
+++ src/Test5.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+
+class Test5 extends Component {
+
+    state ={
+        value: null
+    }
+
+    componentDidMount(){
+        axios.get('https://raw.githubusercontent.com/cindyiskandar/Demo-Repo/new_commit/carbon_data.json')
+        .then(response =>  {
+            var value = response.data;
+            this.setState({ value });
+        });
+    }
+
+    render() {
+        return (
+            <div className="center">
+                <p className="parag">The estimated carbon emissions saved by the array for the day are {this.state.value} gCO2.</p>
+            </div>
+        )
+    }
+}
+
+export default Test5;
+
+
+
